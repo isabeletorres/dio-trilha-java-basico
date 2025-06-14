69f59c5e3d1e191a0b21637a1413217b3d89596f
Isabele Torres <isabeledasilvatorres@gmail.com>
2025-06-14 10:12:05 -0300

    Adicinando código completo com resolução do problema Conta Banco

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class Main {
+import java.util.*;
+
+public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Digite o número da agência: ");
+        String numeroAgencia = sc.nextLine();
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.print("Digite o Saldo da conta do Cliente: ");
+        double saldo = sc.nextDouble();
+
+        System.out.println();
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, numeroAgencia, numeroConta, saldo);
+
+        sc.close();
     }
 }
